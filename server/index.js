9e34a4efe6389ef781cbe307bf9b7a6c10e91451
Nika Lukava <Nikolla-L2002@users.noreply.github.com>
2022-03-05 12:37:21 +0400

    add redux, setup database, make posts add form

--- server/index.js
+++ server/index.js
@@ -2,16 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30bm", extended: true}));
 app.use(cors());
+app.use('/posts', postRoutes);
 
 const connection_url = 'mongodb+srv://nikolla:12341234@nikoloza.84pn3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const port = process.env.PORT || 3001;
+
 
 mongoose.connect(connection_url)
     .then(() => app.listen(port, () => console.log(`Server is running on ${port}`)))
