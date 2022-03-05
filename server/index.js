cd63e3c756f51caaad567171443fb696cd40f970
Nika Lukava <Nikolla-L2002@users.noreply.github.com>
2022-03-05 09:31:58 +0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30bm", extended: true}));
+app.use(cors());
+
+const connection_url = 'mongodb+srv://nikolla:12341234@nikoloza.84pn3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const port = process.env.PORT || 3001;
+
+mongoose.connect(connection_url)
+    .then(() => app.listen(port, () => console.log(`Server is running on ${port}`)))
+    .catch((err) => console.log(err.message));
